c09a48147973450795585e6d3f758ed89a06a81a
Tom Hames <40021462+TomHames@users.noreply.github.com>
2018-08-10 14:49:38 +0100

    Update and rename Content.js to WeatherData.js

--- src/Components/WeatherData.js
+++ src/Components/WeatherData.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Content extends React.Component {
+export default class WeatherData extends React.Component {
   render() {
     return (
       <div>
